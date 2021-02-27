9978b1569958b4fd61a2a9b7bfba188bd352a865
Jared Weiss <jared.lee.weiss@gmail.com>
2021-02-27 11:36:01 -0700

    cleaning up code and getting game_fire() to work. added mutex locking to handle_client_connect()

--- battlebit/src/server.c
+++ battlebit/src/server.c
@@ -20,6 +20,8 @@
 #define SERVER_FAIL -2
 
 static game_server *SERVER;
+pthread_mutex_t lock;
+struct game* CURR_GAME;
 
 void init_server() {
     if (SERVER == NULL) {
@@ -29,8 +31,6 @@ void init_server() {
     }
 }
 
-pthread_mutex_t lock;
-struct game* CURR_GAME;
 void* handle_client_connect(void* arg) {
     // STEP 8 - This is the big one: you will need to re-implement the REPL code from
     // the repl.c file, but with a twist: you need to make sure that a player only
@@ -47,71 +47,99 @@ void* handle_client_connect(void* arg) {
     // be working against network sockets rather than standard out, and you will need
     // to coordinate turns via the game::status field.
     CURR_GAME == NULL ? game_init() : printf("");
-    CURR_GAME = game_get_current();
-    long player = (long) arg; // retrieve player number from the garbo api
-    // player is coming out as 1 for some reason on the first connection
-    struct char_buff* buffer = cb_create(2000); // create a buffer that we can work with
-    // default strings
-    char* prompt = "battleBit (? for help) > ";
-    char* help_str = "? - show help\nload <string> - load a ship layout file for the given player \\n"
+    long player = (long) arg;
+    int player_turn_status = !player ? PLAYER_0_TURN : PLAYER_1_TURN;
+    int other_player_status = player ? PLAYER_0_TURN : PLAYER_1_TURN;
+    struct char_buff* buffer = cb_create(100); // create a buffer that we can work with
+    const char* prompt = "battleBit (? for help) > ";
+    const char* help_str = "? - show help\nload <string> - load a ship layout file for the given player\n"
                      "show - shows the board for the given player\n"
                      "fire [0-7] [0-7] - fire at the given position\n"
                      "say <string> - Send the string to all players as part of a chat\n"
                      "reset - reset the game\n"
                      "exit - quit the server\n";
-    printf("Player init: %ld\n", player);
     while(1) {
+        /* Setup variables, update game and reset the buffer */
+        CURR_GAME = game_get_current();
         cb_reset(buffer);
         send(SERVER->player_sockets[player], prompt, strlen(prompt), 0);
-        /*ssize_t bytes =*/recv(SERVER->player_sockets[player], buffer->buffer, buffer->size, 0);
-        printf("RECV SUCCESS - received: %s", buffer->buffer);
+        recv(SERVER->player_sockets[player], buffer->buffer, buffer->size, 0);
+        /* parse user input */
         char *command = cb_tokenize(buffer, " \n\r");
-        printf("command: %s<endstr>\n", command);
         if (command) {
             char *arg1 = cb_next_token(buffer);
             char *arg2 = cb_next_token(buffer);
-            char *arg3 = cb_next_token(buffer);
             if (strcmp(command, "exit") == 0) {
-                printf("Player %ld disconnect\n", player);
+                pthread_mutex_lock(&lock);
                 send(SERVER->player_sockets[player], "Goodbye!\n", strlen("Goodbye!\n"), 0);
                 close(SERVER->player_sockets[player]);
+                pthread_mutex_unlock(&lock);
                 break;
             } else if (strcmp(command, "?") == 0) {
                 send(SERVER->player_sockets[player], help_str, strlen(help_str), 0);
             } else if (strcmp(command, "show") == 0) {
 
+                pthread_mutex_lock(&lock);
                 struct char_buff *boardBuffer = cb_create(2000);
                 repl_print_board(CURR_GAME, (int) player, boardBuffer);
                 send(SERVER->player_sockets[player], boardBuffer->buffer, boardBuffer->size, 0);
                 cb_free(boardBuffer);
+                pthread_mutex_unlock(&lock);
 
             } else if (strcmp(command, "reset") == 0) {
 
+                pthread_mutex_lock(&lock);
                 game_init();
+                pthread_mutex_unlock(&lock);
 
             } else if (strcmp(command, "load") == 0) {
 
-                game_load_board(game_get_current(), (int) player, arg1);
+                pthread_mutex_lock(&lock);
+                game_load_board(CURR_GAME, (int) player, arg1);
+                pthread_mutex_unlock(&lock);
 
             } else if (strcmp(command, "fire") == 0) {
                 int x = atoi(arg1);
                 int y = atoi(arg2);
-                if (x < 0 || x >= BOARD_DIMENSION || y < 0 || y >= BOARD_DIMENSION) {
-                    printf("Invalid coordinate: %i %i\n", x, y);
-                } else {
-                    printf("Player %i fired at %i %i\n", player + 1, x, y);
-                    int result = game_fire(game_get_current(), player, x, y);
-                    if (result) {
-                        printf("  HIT!!!");
+                pthread_mutex_lock(&lock);
+                if (CURR_GAME->status == player_turn_status) {
+
+                    if (x < 0 || x >= BOARD_DIMENSION || y < 0 || y >= BOARD_DIMENSION) {
+                        send(SERVER->player_sockets[player], "Invalid coordinate: %i %i\n",
+                             strlen("Invalid coordinate: %i %i\n"), 0);
                     } else {
-                        printf("  Miss");
+                        char_buff* fire_buff = cb_create(50);
+                        sprintf(fire_buff->buffer, "\nPlayer %ld fired at %i %i\n", player + 1, x, y);
+                        int result = game_fire(CURR_GAME, (int) player, x, y);
+                        server_broadcast(fire_buff);
+                        cb_reset(fire_buff);
+                        if (result) {
+                            cb_append(fire_buff, "\tHit!!!\n");
+                            server_broadcast(fire_buff);
+                        } else {
+                            cb_append(fire_buff, "\tMiss\n");
+                            server_broadcast(fire_buff);
+                        }
+                        cb_reset(fire_buff);
+                        cb_append(fire_buff, prompt);
+                        server_broadcast(fire_buff);
+                        cb_free(fire_buff);
                     }
+                    CURR_GAME->status = other_player_status;
+
                 }
+                else {
+                    send(SERVER->player_sockets[player], "It isn't your turn!\n",
+                         strlen("It isn't your turn!\n"), 0);
+                }
+                pthread_mutex_unlock(&lock);
             } else {
+                pthread_mutex_lock(&lock);
                 struct char_buff *unknown_buff = cb_create(30);
                 sprintf(unknown_buff->buffer, "Unknown Command: %s\n", command);
                 send(SERVER->player_sockets[player], unknown_buff->buffer, unknown_buff->size, 0);
                 cb_free(unknown_buff);
+                pthread_mutex_unlock(&lock);
             }
         }
     }
@@ -133,38 +161,32 @@ void* run_server(void* arg) {
     //
     // You will then create a thread running handle_client_connect, passing the player number out
     // so they can interact with the server asynchronously*/
-    printf("Server init.\n");
     int server_socket_fd = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
     if (server_socket_fd == -1) {
         perror("ERROR | Server socket initialization failed\n");
         exit(SOCKET_INIT_FAIL);
     }
-    SERVER->server_socket = server_socket_fd;
     int yes = 1;
     setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
-    struct sockaddr_in server;
 
+    // ??????
+    struct sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(BATTLEBIT_PORT);
 
-    printf("Server bind.\n");
     if (bind(server_socket_fd, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        // bind failed
         perror("ERROR | Server bind failed\n");
         exit(SERVER_FAIL);
     }
     else {
-        // bind success, listen for incoming connections
         listen(server_socket_fd, 2);
         struct sockaddr_in client; socklen_t size_from_connect; int client_socket_fd;
-        long requests = 0;
+        long requests = 0; // long because it is 8 bytes (same as void*, avoids weird casting issues)
         while ((client_socket_fd = accept(server_socket_fd,
                                           (struct sockaddr *) &client,
-                                          &size_from_connect)) > 0) {
-            // on connect, add the socket to the player_sockets array
+                                          &size_from_connect)) > 0 && requests < 2)   {
             SERVER->player_sockets[requests] = client_socket_fd;
-            // initialize a thread for the player
             pthread_t player_thread;
             pthread_create(&player_thread, NULL, handle_client_connect, (void*)requests);
             SERVER->player_threads[requests] = player_thread;
@@ -183,6 +205,5 @@ int server_start() {
     pthread_t server_thread;
     pthread_create(&server_thread, NULL, run_server, NULL);
     SERVER->server_thread = server_thread;
-    pthread_join(server_thread, NULL);
     return 0;
 }
